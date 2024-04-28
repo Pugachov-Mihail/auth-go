d4ab5920cc83d5e78bc044f42e935446da593477
Mihail Pugachev <xd393727@gmail.com>
2024-04-28 16:33:52 +0300

    finish work reset api

--- internal/storage/reset/reset_storage.go
+++ internal/storage/reset/reset_storage.go
@@ -5,7 +5,9 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"golang.org/x/crypto/bcrypt"
 	"log"
+	"time"
 )
 
 type StorageReset struct {
@@ -27,19 +29,64 @@ func New(storagePath configapp.ConfigDB) (*StorageReset, error) {
 }
 
 func (s *StorageReset) Email(ctx context.Context, email string, uid int64) (int64, error) {
-	query := `SELECT id FROM users_my WHERE id=5;`
+	query := `UPDATE users_my SET email=$1 WHERE id=$2;`
+	fu := `SELECT id FROM users_my WHERE email=$1;`
+
+	dbCtx, cancel := context.WithTimeout(ctx, time.Second*5)
+	defer cancel()
 
 	var user uint64
 
-	err := s.db.QueryRowContext(ctx, query).Scan(&user)
+	err := s.db.QueryRowContext(dbCtx, query, email, uid)
 	if err != nil {
 		return 0, fmt.Errorf("ошибка смены почты: %w ", err)
 	}
+	if err := s.db.QueryRowContext(dbCtx, fu, email).Scan(&user); err != nil {
+		return 0, fmt.Errorf("ошибка получения пользователя по новой почте: %w", err)
+	}
+
 	return int64(user), nil
 }
 func (s *StorageReset) Password(ctx context.Context, password string, userId int64) (int64, error) {
-	panic(ctx)
+	query := `UPDATE users_my SET password=$1 WHERE id=$2;`
+	fu := `SELECT id FROM users_my WHERE id=$1;`
+
+	passHash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return 0, fmt.Errorf("ошибка генерации хеша пароля: %w", err)
+	}
+
+	dbCtx, cancel := context.WithTimeout(ctx, time.Second*5)
+	defer cancel()
+
+	var user uint64
+
+	if err := s.db.QueryRowContext(dbCtx, query, passHash, userId); err != nil {
+		return 0, fmt.Errorf("ошибка смены пароля: %v", err)
+	}
+
+	if err := s.db.QueryRowContext(dbCtx, fu, userId).Scan(&user); err != nil {
+		return 0, fmt.Errorf("ошибка получения пользователя при смене пароля: %w", err)
+	}
+
+	return int64(user), nil
 }
 func (s *StorageReset) IdSteam(ctx context.Context, steamId int64, userId int64) (int64, error) {
-	panic(ctx)
+	query := `UPDATE users_my SET steam_id=$1 WHERE id=$2;`
+	fu := `SELECT id FROM users_my WHERE id=$1;`
+
+	dbCtx, cancel := context.WithTimeout(ctx, time.Second*5)
+	defer cancel()
+
+	var user uint64
+
+	err := s.db.QueryRowContext(dbCtx, query, steamId, userId)
+	if err != nil {
+		return 0, fmt.Errorf("ошибка смены Steam Id: %v ", err)
+	}
+	if err := s.db.QueryRowContext(dbCtx, fu, userId).Scan(&user); err != nil {
+		return 0, fmt.Errorf("ошибка получения Steam Id: %w", err)
+	}
+
+	return int64(user), nil
 }
